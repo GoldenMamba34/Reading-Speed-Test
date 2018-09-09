57ddb52a00e4ef1bbc2ba346283d004157f834ba
Andrew Courey <goldenmambasoft@gmail.com>
2018-09-09 18:33:08 -0400

    Version 2

--- ReadingSpeedTest.js
+++ ReadingSpeedTest.js
@@ -1,10 +1,10 @@
 
-text = document.getElementById("text").innerText
-startButton = document.getElementById("start")
-doneButton = document.getElementById("done")
-resetButton = document.getElementById("reset")
-changeText = document.getElementById("changeText");
-
+let text = document.getElementById("text").innerText
+let startButton = document.getElementById("start")
+let doneButton = document.getElementById("done")
+let resetButton = document.getElementById("reset")
+let changeText = document.getElementById("changeText");
+let localStorage = window.localStorage;
 var start = 0;
 var isTesting = false;
 
@@ -15,7 +15,11 @@ function WordCount(str) {
 }
 
 
-
+document.addEventListener("DOMContentLoaded", () => {
+    if (  localStorage.getItem('text')) {
+        document.getElementById("text").innerText = localStorage.getItem('text');
+    }
+})
 startButton.addEventListener("click", function(){
     start = Date.now()
     startButton.disabled = true;
@@ -30,32 +34,35 @@ startButton.addEventListener("click", function(){
 
 doneButton.addEventListener("click", function(){
     if (isTesting) {
-    var textLength = WordCount(text)
+    var textLength = WordCount(text);
     var milliseconds = Date.now() - start;
-    var seconds = milliseconds / 1000
-    var minutes = seconds / 60
-    var wordsPerMinute = textLength / minutes
+    var seconds = milliseconds / 1000;
+    var minutes = seconds / 60;
+    var wordsPerMinute = textLength / minutes;
 
    doneButton.opacity = 0.2;
-   doneButton.innerText = "You can read " + Math.round(wordsPerMinute) + " words per minute";
-   doneButton.style.width = "80%"
-   doneButton.style.marginLeft = "10%"
-   doneButton.style.marginRight = "10%"
-doneButton.style.transitionDuration = ".5s"
+   doneButton.innerText = `You can read ${Math.round(wordsPerMinute)} words per minute`;
+   doneButton.style.width = "80%";
+   doneButton.style.marginLeft = "10%";
+   doneButton.style.marginRight = "10%";
+doneButton.style.transitionDuration = ".5s";
 doneButton.disabled = true;
 changeText.disabled = false;
 changeText.style.opacity = 1;
+
+
+
     }
 })
 
 
 resetButton.addEventListener("click", function(){
     doneButton.style.opacity = 0.2;
-    doneButton.innerText = "Done"
-    doneButton.style.width = "50%"
-    doneButton.style.marginLeft = "25%"
-    doneButton.style.marginRight = "25%"
- doneButton.style.transitionDuration = ".3s"
+    doneButton.innerText = "Done";
+    doneButton.style.width = "50%";
+    doneButton.style.marginLeft = "25%";
+    doneButton.style.marginRight = "25%";
+ doneButton.style.transitionDuration = ".3s";
  doneButton.disabled = true;
 
  startButton.disabled = false;
@@ -79,4 +86,6 @@ $("#doneWithChangeText").click(() => {
     text = $("#changeTextTextArea").val();
     $("#overlay").hide(500);
     document.getElementById("text").innerText = $("#changeTextTextArea").val();
+    localStorage.setItem('text', text);
+    
 })
