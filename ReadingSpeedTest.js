65dd5d8486b987754c62d32a903c4e133cbc43af
Andrew Courey <goldenmambasoft@gmail.com>
2018-09-09 09:52:16 -0400

    Version 1

---
+++ ReadingSpeedTest.js
@@ -0,0 +1,82 @@
+
+text = document.getElementById("text").innerText
+startButton = document.getElementById("start")
+doneButton = document.getElementById("done")
+resetButton = document.getElementById("reset")
+changeText = document.getElementById("changeText");
+
+var start = 0;
+var isTesting = false;
+
+function WordCount(str) {
+    return str.split(' ')
+    .filter(function(n) { return n != '' })
+    .length
+}
+
+
+
+startButton.addEventListener("click", function(){
+    start = Date.now()
+    startButton.disabled = true;
+    startButton.style.opacity = 0.2;
+    doneButton.style.opacity = 1;
+    doneButton.disabled = false;
+    isTesting = true;
+    changeText.disabled = true;
+    changeText.style.opacity = 0.2;
+
+})
+
+doneButton.addEventListener("click", function(){
+    if (isTesting) {
+    var textLength = WordCount(text)
+    var milliseconds = Date.now() - start;
+    var seconds = milliseconds / 1000
+    var minutes = seconds / 60
+    var wordsPerMinute = textLength / minutes
+
+   doneButton.opacity = 0.2;
+   doneButton.innerText = "You can read " + Math.round(wordsPerMinute) + " words per minute";
+   doneButton.style.width = "80%"
+   doneButton.style.marginLeft = "10%"
+   doneButton.style.marginRight = "10%"
+doneButton.style.transitionDuration = ".5s"
+doneButton.disabled = true;
+changeText.disabled = false;
+changeText.style.opacity = 1;
+    }
+})
+
+
+resetButton.addEventListener("click", function(){
+    doneButton.style.opacity = 0.2;
+    doneButton.innerText = "Done"
+    doneButton.style.width = "50%"
+    doneButton.style.marginLeft = "25%"
+    doneButton.style.marginRight = "25%"
+ doneButton.style.transitionDuration = ".3s"
+ doneButton.disabled = true;
+
+ startButton.disabled = false;
+ startButton.style.opacity = 1;
+ isTesting = false;
+ $("html, body").animate({ scrollTop: 0 }, 400);
+ changeText.disabled = false;
+changeText.style.opacity = 1;
+})
+
+
+changeText.addEventListener("click", () => {
+    $("#overlay").show(500);
+})
+
+$("#exitPopup").click(() => {
+    $("#overlay").hide(500);
+})
+
+$("#doneWithChangeText").click(() => {
+    text = $("#changeTextTextArea").val();
+    $("#overlay").hide(500);
+    document.getElementById("text").innerText = $("#changeTextTextArea").val();
+})
